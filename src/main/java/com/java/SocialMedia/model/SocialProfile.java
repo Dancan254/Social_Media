02bf25d26748267f2202eed4348d843d4e53a78f
Dancan254 <dancanian25@gmail.com>
2024-10-20 00:35:42 +0300

    chore: Update SocialProfile and SocialUser model classes with their respective mappings

--- src/main/java/com/java/SocialMedia/model/SocialProfile.java
+++ src/main/java/com/java/SocialMedia/model/SocialProfile.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,8 +18,7 @@ public class SocialProfile {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
-    @JoinColumn(name = "social_user")
+    @OneToOne(mappedBy = "profile")
     private SocialUser user;
 
 }
--- src/main/java/com/java/SocialMedia/model/SocialUser.java
+++ src/main/java/com/java/SocialMedia/model/SocialUser.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,4 +19,8 @@ public class SocialUser {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    //assuming that the social user owns the relationship
+    @OneToOne
+    @JoinColumn(name = "social_profile")
+    private SocialProfile profile;
 }
