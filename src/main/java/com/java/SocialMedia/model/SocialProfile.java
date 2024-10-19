4ec7fc656d03ac1672fb4d2eeb4756d1b3804027
Dancan254 <dancanian25@gmail.com>
2024-10-20 00:13:35 +0300

    feat: Add SocialProfile model class

---
+++ src/main/java/com/java/SocialMedia/model/SocialProfile.java
@@ -0,0 +1,26 @@
+package com.java.SocialMedia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SocialProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "social_user")
+    private SocialUser user;
+
+}
