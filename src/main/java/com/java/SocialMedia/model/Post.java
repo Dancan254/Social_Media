3c8ddf4cc588771f20d4f407c3be6f9c34cca58c
Dancan254 <dancanian25@gmail.com>
2024-10-21 08:25:32 +0300

    chore: Add author relationship to Post model

--- src/main/java/com/java/SocialMedia/model/Post.java
+++ src/main/java/com/java/SocialMedia/model/Post.java
@@ -1,5 +1,24 @@
 package com.java.SocialMedia.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
 public class Post {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    //assuming that the author owns the relationship
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private SocialUser author;
 }
