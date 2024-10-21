0bd6f0dac24e50cdcaf57436f41da3675b50885f
Dancan254 <dancanian25@gmail.com>
2024-10-21 19:50:26 +0300

    refactor: Update Post model class with author relationship

--- src/main/java/com/java/SocialMedia/model/Post.java
+++ src/main/java/com/java/SocialMedia/model/Post.java
@@ -1,24 +1,29 @@
 package com.java.SocialMedia.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String content;
-
     //assuming that the author owns the relationship
     @ManyToOne
     @JoinColumn(name = "author_id")
+    @JsonIgnore
     private SocialUser author;
 }
