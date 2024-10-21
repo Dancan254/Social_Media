71c642e0bd04edc166c645ba7888e400391abcb4
Dancan254 <dancanian25@gmail.com>
2024-10-21 19:50:12 +0300

    chore: Update Groups model class with members field and exclude it from equals and hashCode methods

--- src/main/java/com/java/SocialMedia/model/Groups.java
+++ src/main/java/com/java/SocialMedia/model/Groups.java
@@ -1,6 +1,9 @@
 package com.java.SocialMedia.model;
 
+import java.util.HashSet;
 import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,11 +11,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
+@EqualsAndHashCode(exclude = "members")
 public class Groups {
 
     @Id
@@ -20,5 +27,6 @@ public class Groups {
     private Long id;
 
     @ManyToMany(mappedBy = "groups")
-    private Set<SocialUser> members;
+    @JsonIgnore
+    private Set<SocialUser> members = new HashSet<>();
 }
