02d6ae3ab94add4d8a1e2382d3b6ec64a0b28237
Dancan254 <dancanian25@gmail.com>
2024-10-21 19:50:04 +0300

    feat: Exclude user field from SocialProfile's equals and hashCode methods

--- src/main/java/com/java/SocialMedia/model/SocialProfile.java
+++ src/main/java/com/java/SocialMedia/model/SocialProfile.java
@@ -1,5 +1,7 @@
 package com.java.SocialMedia.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,18 +9,22 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(exclude = "user")
 public class SocialProfile {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToOne(mappedBy = "profile")
+    @JsonIgnore
     private SocialUser user;
 
 }
